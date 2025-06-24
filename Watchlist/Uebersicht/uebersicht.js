f786105d0ae273040498bf5477dea5235aa45ad4
Antonia <anrybaczyk@gmail.com>
2025-06-24 19:30:03 +0200

    fix

---
+++ Watchlist/Uebersicht/uebersicht.js
@@ -0,0 +1,29 @@
+{
+    "uebersicht";[
+        {
+            "name": "Greys Anatomy",
+            "staffeln": 22,
+            "episoden": 448,
+            "streamen": "Netflix",
+            "genre": "Drama",
+            "bewertung": 10,
+        },
+        {
+            "name": "Bridgeton",
+            "staffeln": 6,
+            "episoden": 46,
+            "streamen": "Netflix",
+            "genre": "Seifenoper",
+            "bewertung": 7,
+        },
+        {
+            "name": "Haus des Geldes",
+            "staffeln": 5,
+            "episoden": 48,
+            "streamen": "Netflix",
+            "genre": "Thriller",
+            "bewertung": 9,
+        }
+
+    ]
+}

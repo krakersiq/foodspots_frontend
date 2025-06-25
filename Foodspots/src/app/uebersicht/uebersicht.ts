50661555cb90ff5b02150baebddbdb396eed86e4
Antonia <anrybaczyk@gmail.com>
2025-06-25 22:18:26 +0200

    add foodspots table

--- Foodspots/src/app/uebersicht/uebersicht.ts
+++ Foodspots/src/app/uebersicht/uebersicht.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Foodspot } from '../shared/foodspot';
+import { Backend } from '../shared/backend';
 
 @Component({
   selector: 'app-uebersicht',
   imports: [],
   templateUrl: './uebersicht.html',
-  styleUrl: './uebersicht.css'
+  styleUrl: './uebersicht.css',
 })
-export class Uebersicht {
+export class Uebersicht implements OnInit {
+  private bs = inject(Backend);
+  foodspots: Foodspot[] = [];
 
+  ngOnInit(): void {
+    this.bs
+      .getAll()
+      .then((response) => (this.foodspots = response))
+      .then((foodspots) =>
+        console.log(' foodspots in TableComponent : ', foodspots)
+      );
+  }
 }

795d0c613a8b20acd2674064ba88ef1fe32de788
Antonia <anrybaczyk@gmail.com>
2025-06-26 13:48:11 +0200

    edit nav

--- Foodspots/src/app/app.routes.ts
+++ Foodspots/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Startseite } from './startseite/startseite';
 import { Uebersicht } from './uebersicht/uebersicht';
+import { Formular } from './formular/formular';
 
 export const routes: Routes = [
     {  path: '', component: Startseite },
-    {  path: 'uebersicht', component: Uebersicht }
+    {  path: 'uebersicht', component: Uebersicht },
+    {  path: 'formular', component: Formular }
 ];
--- Foodspots/src/app/app.ts
+++ Foodspots/src/app/app.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { routes } from './app.routes';
 import { Startseite } from './startseite/startseite';
+import { Uebersicht } from './uebersicht/uebersicht';
+import { Formular } from './formular/formular';
 
 @Component({
   selector: 'app-root',

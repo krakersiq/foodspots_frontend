79b516f0062bccc22cdbe40d758304a51f37921e
Antonia <anrybaczyk@gmail.com>
2025-07-01 12:22:23 +0200

    fix

--- Foodspots/src/app/formular/formular.ts
+++ Foodspots/src/app/formular/formular.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formular',
-  imports: [],
+  /* imports: [], */
   templateUrl: './formular.html',
   styleUrl: './formular.css'
 })
@@ -12,6 +12,7 @@ export class Formular {
   constructor(private router: Router) {}
 
   onSubmit() {
+    console.log('Formular wird angezeigt');
     this.router.navigate(['/formular']);
   }
 

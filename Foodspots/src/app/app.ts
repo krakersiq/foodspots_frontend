1629bb38afdccc8cfde9df9545b0942adf34ad3e
Antonia <anrybaczyk@gmail.com>
2025-07-16 00:29:37 +0200

    add imports

--- Foodspots/src/app/app.ts
+++ Foodspots/src/app/app.ts
@@ -5,17 +5,13 @@ import { Startseite } from './startseite/startseite';
 import { Uebersicht } from './uebersicht/uebersicht';
 import { Formular } from './formular/formular';
 import { Registrierung } from './registrierung/registrierung';
+import { Bearbeiten } from './bearbeiten/bearbeiten';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterModule,
-    Startseite,
-    Uebersicht,
-    Formular,
-    Registrierung
-  ],
+  imports: [RouterModule, Startseite, Uebersicht, Formular, Bearbeiten ], /* , Registrierung, Login */
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })

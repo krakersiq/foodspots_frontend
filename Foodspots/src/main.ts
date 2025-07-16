0e8f0e91373ed0d95f3920a6fb67d432404af325
Antonia <anrybaczyk@gmail.com>
2025-07-16 23:15:41 +0200

    fix

--- Foodspots/src/main.ts
+++ Foodspots/src/main.ts
@@ -2,6 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { Formular } from './app/formular/formular';
-import { Registrierung } from './app/registrierung/registrierung';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));

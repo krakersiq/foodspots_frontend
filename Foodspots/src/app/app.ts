bff6036b25b31ea2aba168f8c0480fd9c2dfe771
Antonia <anrybaczyk@gmail.com>
2025-07-16 23:12:03 +0200

    fix

--- Foodspots/src/app/app.ts
+++ Foodspots/src/app/app.ts
@@ -4,17 +4,16 @@ import { routes } from './app.routes';
 import { Startseite } from './startseite/startseite';
 import { Uebersicht } from './uebersicht/uebersicht';
 import { Formular } from './formular/formular';
-import { Registrierung } from './registrierung/registrierung';
 import { Bearbeiten } from './bearbeiten/bearbeiten';
-import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, Startseite, Uebersicht, Formular, Bearbeiten ], /* , Registrierung, Login */
+  imports: [RouterModule, Startseite, Uebersicht, Formular, Bearbeiten ],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
+
 export class App {
   protected title = 'Die besten Foodspots der Welt';
 }
